685673b97ccf9456938290a896443b2d919769d3
kilen0x <fmaloj15@gmial.com>
2024-03-23 16:05:42 +0300

    commit

---
+++ gui.py
@@ -0,0 +1,105 @@
+
+import pygame as pg
+
+
+class Selector:
+        def __init__(self, x, y, width, height, font, color, text = "", view = None, textColor = (255,255,255)):
+            o = len(text) // 15 + 1
+            if o != 0:
+                height *= o
+            self.rect = pg.Rect(x,y,width,height)
+            self.view = view
+            self.color = color
+            self.font = font
+            self.textcolor = textColor
+            
+            ## text ##
+            self.textpos = (self.rect.x+10, self.rect.y +5)
+            p = 0
+            orig = [""]
+            f = 0
+            for i in text:
+                if p % 16 == 0 and p != 0:
+                    orig.append("")
+                    f += 1
+                    orig[f] += i
+                else:
+                    orig[f] += i
+                p += 1
+            self.text = orig
+
+class Gui:
+
+    def __init__(self, SCREEN_WIDTH, SCREEN_HEIGHT, displayName = "main gui", font = None):
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pg.display.set_caption(displayName)
+
+        pg.init()
+        pg.font.init()
+
+        self.sW = SCREEN_WIDTH
+        self.sH = SCREEN_HEIGHT
+        self.fS = 30
+        self.selectors = []
+        self.selectorspages = []
+
+        ## DISPLAY DESIGN ##
+
+        self.font = pg.font.Font(font, self.fS)
+        self.frame_1 = pg.Rect(0, 0, SCREEN_WIDTH//5.13, SCREEN_HEIGHT)
+        self.frame_2 = pg.Rect(300, 0, SCREEN_WIDTH-SCREEN_WIDTH//5.13, SCREEN_HEIGHT)
+
+        self.splitline = pg.Rect(SCREEN_WIDTH//5.40, 0, SCREEN_WIDTH//102, SCREEN_HEIGHT)
+
+        ## COLORS ##
+
+        self.frame_1_color = (36,36,36)
+        self.frame_2_color = (71,71,71)
+        self.splitline_color = (20,20,20)
+        self.selector_color = (80,80,80)
+
+    def update(self):
+        ...
+        
+
+    def draw(self):
+
+        self.screen.fill(self.frame_2_color)
+        
+        pg.draw.rect(self.screen, self.frame_1_color, self.frame_1)
+        pg.draw.rect(self.screen, self.frame_2_color, self.frame_2)
+        pg.draw.rect(self.screen, self.splitline_color, self.splitline)
+
+        for selector in self.selectors:
+            pg.draw.rect(self.screen, selector.color, selector.rect, border_radius=20)
+            pg.draw.rect(self.screen, (0,0,0), selector.rect, width = 2, border_radius=20)
+            y = 0
+            for i in selector.text:
+                text = selector.font.render(i, True, selector.textcolor)
+                p = [selector.textpos[0], selector.textpos[1]]
+                p[1] += y
+                self.screen.blit(text, p)
+                y += 30 # gap
+
+
+        pg.display.flip()
+
+    def event(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                quit()
+
+    def main_loop(self):
+        while True:
+            self.update()
+            self.draw()
+            self.event()
+
+
+    def add_selector(self, selectorText):
+        y = 30 + len(self.selectors) * 150
+        print(y)
+        selector = Selector(30, y, 200, 30, self.font, self.selector_color, selectorText, str(len(self.selectors)))
+
+        self.selectors.append(selector)
+
